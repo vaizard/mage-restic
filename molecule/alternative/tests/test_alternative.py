d2b95ea407aa28f71c455b7c6186a6f8c7e5fd60
Paweł Krupa <paulfantom@gmail.com>
2018-05-14 15:01:19 +0200

    allow specifying `latest` as version (#19)
    * add new test scenario; allow specifying `latest` as version
    
    * properly execute preflight checks
    
    * remove pesky tabs
    
    * fix wrong variable name
    
    * fix alternative tests
    
    * ask github API only once

---
+++ molecule/alternative/tests/test_alternative.py
@@ -0,0 +1,32 @@
+import os
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_binary(host):
+    binary = host.file("/usr/local/bin/restic")
+    assert binary.exists
+    assert binary.is_file
+    assert binary.mode == 0o750
+
+
+def test_cronfiles(host):
+    cronfiles = [
+        "/etc/cron.d/restic-backblaze-example",
+        "/etc/cron.d/restic-s3-example"
+    ]
+    for file in cronfiles:
+        f = host.file(file)
+        assert f.exists
+        assert f.is_file
+        assert f.mode == 0o640
+        with host.sudo():
+            assert f.contains('RESTIC_PASSWORD="correcthorsebatterystaple"')
+#
+#
+# def test_logdir(host):
+#     logdir = host.file("/var/log/restic")
+#     assert logdir.is_directory
+#     assert logdir.exists
